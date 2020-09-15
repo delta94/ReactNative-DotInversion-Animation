5fd774861087f335400a3647c129fb373c4ecad0
Hussain Feroz <hussainferoz839@gmail.com>
2020-09-16 01:41:10 +0500

    animation added

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
+
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
-import {StyleSheet, View, StatusBar, TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  StatusBar,
+  TouchableOpacity,
+  Animated,
+} from 'react-native';
+
+const {Value, timing} = Animated;
 
 const App = () => {
+  const [value, setValue] = useState(0);
+  const animation = useRef(new Value(0)).current;
+
+  const rotateY = animation.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: ['0deg', '-90deg', '-180deg'],
+  });
+
+  const scale = animation.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [1, 8, 1],
+  });
+
+  const translateX = animation.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [0, 50, 0],
+  });
+
+  const backgroundColor = animation.interpolate({
+    inputRange: [0, 0.5, 0.501, 1],
+    outputRange: ['#F576AA', '#F576AA', '#000B92', '#000B92'],
+  });
+
+  const circleBackgroundColor = animation.interpolate({
+    inputRange: [0, 0.5, 0.501, 1],
+    outputRange: ['#000B92', '#000B92', '#F576AA', '#F576AA'],
+  });
+
+  const opacity = animation.interpolate({
+    inputRange: [0, 0.1, 0.9, 1],
+    outputRange: [1, 0, 0, 1],
+  });
+
+  const buttonHandler = (number) => {
+    setValue(number);
+    timing(animation, {
+      toValue: number,
+      duration: 2000,
+      useNativeDriver: false,
+    }).start();
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      <View style={styles.circleContainer}>
-        <View style={[styles.circle]}>
-          <TouchableOpacity style={styles.circle}>
-            <View style={[styles.circle, styles.button]}>
-              <AntDesign name="arrowright" size={28} color="white" />
-            </View>
+      <Animated.View style={[styles.circleContainer, {backgroundColor}]}>
+        <Animated.View
+          style={[
+            styles.circle,
+            {
+              backgroundColor: circleBackgroundColor,
+              transform: [{perspective: 100}, {scale}, {rotateY}, {translateX}],
+            },
+          ]}>
+          <TouchableOpacity
+            style={styles.circle}
+            onPress={() => buttonHandler(value === 0 ? 1 : 0)}>
+            <Animated.View style={[styles.circle, styles.button, {opacity}]}>
+              <AntDesign name="right" size={28} color="white" />
+            </Animated.View>
           </TouchableOpacity>
-        </View>
-      </View>
+        </Animated.View>
+      </Animated.View>
     </View>
   );
 };
@@ -26,13 +86,11 @@ const styles = StyleSheet.create({
   },
   circleContainer: {
     flex: 1,
-    backgroundColor: '#fff',
     justifyContent: 'flex-end',
     alignItems: 'center',
     paddingBottom: 100,
   },
   circle: {
-    backgroundColor: '#444',
     width: 100,
     height: 100,
     borderRadius: 50,
